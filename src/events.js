34e7c5c864ba6f2612049d366b383e5ee7dd812a
Cindy-bio <cindylorraine231@gmail.com>
2025-08-04 13:28:13 +0200

    Added a Js function for the AI generator

--- src/events.js
+++ src/events.js
@@ -1,1 +1,15 @@
+function generatePoem(event) {
+    event.preventDefault();
 
+    let poemContainerElement = document.querySelector('#poemContainer');
+    new Typewriter("#poemContainer", {
+      strings: "This poem is inspired by your favorite romance novel and will be generated shortly...",
+      autoStart: true,
+      cursor: "",
+      delay: 5,
+    });
+   
+}
+
+let poemFormElement = document.querySelector('#poemForm');
+poemFormElement.addEventListener('submit', generatePoem);
